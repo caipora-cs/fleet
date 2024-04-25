b155f42b874b170bf55f24c6d84a2eff0b6b952e
caipora-cs <0xaoi@protonmail.com>
2024-04-25 14:26:12 +0100

    Model for data objects going into DB.

--- TODO.md
+++ TODO.md
@@ -20,6 +20,8 @@ Sniper Features:
             - 5 min from creation (determine from backtesting)
             - find a way to determine wich ones are long term holders
                 - Scrape a website/ telegram/ twitter for the coin
+        -HOLD:
+            - Make a mini script that check the coin DexScreener page for a website span
 
 - Add api calls to the bot for controlling it on the cloud   
     - API Gateway
@@ -34,6 +36,38 @@ recon.py:
         - Return the coins that are good
         - Save security and screnner info about this coins on DB
     - Create a module where it goes and get updated info on the coins already in the DB after 10 min, 1h, 1d, 1w and add to the info already there
+
+    My data squema for the DB:
+    {
+        "pairId": "0xaFafFc47072FD2ac75e15D0CA4700852e293725f",
+        "tokens": ["0x92Ea8f2b711076cC08a734a8e9cD57D3963EbfFf", "0x4200000000000000000000000000000000000006"],
+        "token0": "0x92Ea8f2b711076cC08a734a8e9cD57D3963EbfFf",
+        "token1": "0x4200000000000000000000000000000000000006",
+        "security": {
+            "transfer_pausable": "0",
+            "trust_list": None
+        },
+        "name": "🐕 Base REX",
+        "symbol": "BREX",
+        "fdv": 18594,
+        "liquidity_usd": 18611.19,
+        "creation_timestamp": 1714043589000,
+        "price_change": {
+            "h1": 0,
+            "h24": 0,
+            "h6": 0,
+            "m5": 0
+        },
+        "volume": {
+            "h1": 0.92,
+            "h24": 0.92,
+            "h6": 0.92,
+            "m5": 0
+        },
+        "time_scanned": "2024-04-25 12:13:09",
+        "buy_signal": false
+        "website": "https://www.brex.com"
+    }
 
 Monitoring and Logging
     -CloudWatch
---
+++ sniper-bot/models/token_model.py
@@ -0,0 +1,84 @@
+import dataclasses
+
+
+@dataclasses.dataclass
+class Security:
+    """Security dataclass for TokenData"""
+
+    is_airdrop_scam: bool
+    is_anti_whale: bool
+    is_blacklisted: bool
+    is_honeypot: bool
+    is_in_dex: bool
+    is_mintable: bool
+    is_proxy: bool
+    is_whitelisted: bool
+    sell_tax: float
+    trading_cooldown: bool
+    transfer_pausable: bool
+
+
+@dataclasses.dataclass
+class PriceChange:
+    """PriceChange dataclass for TokenData"""
+
+    h1: float
+    h24: float
+    h6: float
+    m5: float
+
+
+@dataclasses.dataclass
+class Volume:
+    """Volume dataclass for TokenData"""
+
+    h1: float
+    h24: float
+    h6: float
+    m5: float
+
+
+@dataclasses.dataclass
+class TokenData:
+    """TokenData dataclass"""
+
+    pairId: str
+    tokens: list
+    token0: str
+    token1: str
+    security: Security
+    name: str
+    symbol: str
+    fdv: int
+    liquidity_usd: float
+    creation_timestamp: int
+    price_change: PriceChange
+    volume: Volume
+    time_scanned: str
+    buy_signal: bool
+    website: str
+
+
+# Example data
+data = {
+    "pairId": "0xaFafFc47072FD2ac75e15D0CA4700852e293725f",
+    "tokens": [
+        "0x92Ea8f2b711076cC08a734a8e9cD57D3963EbfFf",
+        "0x4200000000000000000000000000000000000006",
+    ],
+    "token0": "0x92Ea8f2b711076cC08a734a8e9cD57D3963EbfFf",
+    "token1": "0x4200000000000000000000000000000000000006",
+    "security": {"transfer_pausable": "0", "trust_list": None},
+    "name": "🐕 Base REX",
+    "symbol": "BREX",
+    "fdv": 18594,
+    "liquidity_usd": 18611.19,
+    "creation_timestamp": 1714043589000,
+    "price_change": {"h1": 0, "h24": 0, "h6": 0, "m5": 0},
+    "volume": {"h1": 0.92, "h24": 0.92, "h6": 0.92, "m5": 0},
+    "time_scanned": "2024-04-25 12:13:09",
+    "buy_signal": False,
+    "website": "https://www.brex.com",
+}
+
+token_data = TokenData(**data)
